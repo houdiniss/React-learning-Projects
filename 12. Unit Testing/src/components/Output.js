d327f3f451f512a82892374a52868e4cda1502bb
Giorgos <giorgosmintis@gmail.com>
2024-06-13 13:25:16 +0300

    added project

---
+++ 12. Unit Testing/src/components/Output.js
@@ -0,0 +1,9 @@
+
+
+function Output(props) {
+  return (
+    <p>{props.children}</p>
+  )
+}
+
+export default Output
---
+++ 12. Unit Testing/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+import Greeting from './components/Greeting';
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
